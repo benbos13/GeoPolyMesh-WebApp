1bf0676b3601455488bd74e61ae1a110acde8bc1
Kevin FEDYNA <fedyna.kevin@gmail.com>
2023-12-19 17:32:36 +0100

    Added progressbar to logger

--- logger.js
+++ logger.js
@@ -18,7 +18,7 @@ export function log(...data) {
             console.log("");
         } else {
             piece = typeof piece == "object" ?  inspect(piece, false, null, true) : piece;
-            console.log(`\x1b[1m[LOG]\x1b[0m `, piece);
+            console.log(`\x1b[1m[LOG]\x1b[0m\t\t`, piece);
         }
     }
 }
@@ -33,7 +33,7 @@ export function warn(...data) {
             console.log("");
         } else {
             piece = typeof piece == "object" ?  inspect(piece, false, null, true) : piece;
-            console.warn(`\x1b[1m\x1b[33m[WARNING]\x1b[0m `, piece);
+            console.warn(`\x1b[1m\x1b[33m[WARNING]\x1b[0m\x1b[33m\t`, piece);
         }
     }
 }
@@ -48,7 +48,7 @@ export function error(...data) {
             console.log("");
         } else {
             piece = typeof piece == "object" ?  inspect(piece, false, null, true) : piece;
-            console.error(`\x1b[1m\x1b[31m[ERROR]\x1b[0m `, piece);
+            console.error(`\x1b[1m\x1b[31m[ERROR]\x1b[0m\x1b[31m\t\t`, piece);
         }
     }
 }
@@ -63,5 +63,28 @@ export function assert(condition, errorClass, message) {
     if (!condition) {
         error("During assertion...", message, "");
         throw errorClass(message);
+    }
+}
+
+/**
+ * Displays a progressbar in the console.
+ * @param {number} progress A number between 0 and total.
+ * @param {number} total The total amout corresponding to 100%.
+ * @param {number} width The with of the progressbar.
+ */
+export function progressbar(progress, total, width) {
+    let done = Math.floor(width * progress / total);
+    let part = Math.floor((width * progress / total - done) * 3);
+    let char = ["░", "▒", "▓"][part];
+    let done_percent = Math.floor(100 * progress / total);
+    
+    if (progress == 0) {
+        console.log(`\x1b[1m\x1b[34m[PROGRESS]\x1b[0m\t \x1b[34m\x1b[1m${"█".repeat(done)}\x1b[0m\x1b[38;5;240m${"█".repeat(width - done)}\x1b[0m  ${done_percent}% `);
+    }
+
+    process.stdout.write(`\r\x1b[1m\x1b[34m[PROGRESS]\x1b[0m\t \x1b[34m\x1b[1m${"█".repeat(done)}${char}\x1b[0m${"░".repeat(width - 1 - done)}\x1b[0m  ${done_percent}% `);
+
+    if (progress >= total) {
+        console.log("");
     }
 }
