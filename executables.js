b2bc477a37260543faf41f3290bb76e68c6eacde
Benjamin Bosco <benjy.bosco13@gmail.com>
2024-06-06 10:51:17 +0200

    Ask to the user if he wants to launch the other exe after the first one is finished

--- executables.js
+++ executables.js
@@ -2,7 +2,6 @@ import path from "path";
 import { exec } from "child_process";
 
 export function Cov2Aniso(executablePath){
-    // Execute the command asynchronously in a new terminal window
     executablePath = path.resolve(`"${executablePath}" + "/Cov2Aniso.exe"`);
     const command = `"${executablePath}"`;
 
@@ -32,27 +31,25 @@ export function Cov2Aniso(executablePath){
 }
 
 export function So2Cov(SoFile, executablePath){
-    // Execute the command asynchronously in a new terminal window
     const filePath = path.resolve(SoFile.path);
     executablePath = `${executablePath}/So2Cov.exe`;
     const command = `"${executablePath}" "${filePath}"`;
 
     let terminalCommand;
-
     switch (process.platform) {
         case 'win32':
-            terminalCommand = `start cmd.exe /k "${command}"`;
+            terminalCommand = `start cmd.exe /k "${command} && echo. && set /p answer=Do you want to launch the other function (Y/N)? && if /i !answer! == Y (echo Launching other function & exit 0) else (exit 1)"`;
             break;
         case 'darwin':
-            terminalCommand = `osascript -e 'tell application "Terminal" to do script "${command}"'`;
+            terminalCommand = `osascript -e 'tell application "Terminal" to do script "${command}; echo; read -p \\"Do you want to launch the other function (Y/N)? \\" answer; if [ \\"$answer\\" = \\"Y\\" ]; then echo \\"Launching other function\\"; exit 0; else exit 1; fi"'`;
             break;
         case 'linux':
-            terminalCommand = `gnome-terminal -- bash -c "${command}; exec bash"`;
+            terminalCommand = `gnome-terminal -- bash -c "${command}; echo; read -p 'Do you want to launch the other function (Y/N)? ' answer; if [ \\"$answer\\" = 'Y' ]; then echo 'Launching other function'; exit 0; else exit 1; fi; exec bash"`;
             break;
         default:
             throw new Error('Unsupported platform: ' + process.platform);
     }
-
+    console.log("terminalCommand: ", terminalCommand);
     exec(terminalCommand, (error) => {
         if (error) {
             console.error(`exec error: ${error}`);
